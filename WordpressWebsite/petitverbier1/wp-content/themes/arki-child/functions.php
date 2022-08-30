34fee9739c33dcc3cad7bfc66aee5fe567753834
BriskstarTeam <sales@briskstartech.com>
2022-08-30 17:19:56 +0530

    changes on project directorys

--- WordpressWebsite/petitverbier1/wp-content/themes/arki-child/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	// start your child theme code here
-add_filter('use_block_editor_for_post', '__return_false', 10);
--- WordpressWebsite/petitverbier2/wp-content/themes/happy-baby-child/js/main.js
+++
@@ -1,3 +0,0 @@
-jQuery( document ).ready(function() {
-    jQuery(".logo_home").attr("href", "/")
-});
